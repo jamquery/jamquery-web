d797b4e13477b19a9268dfd5b55965df4b079850
tura <soldier4443@gmail.com>
2018-12-11 01:14:26 +0900

    mysql database 연결해서 가져오는 부분 만듦

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-// 1. Get ready
+// Setup express
 var express = require("express");
 var app = express();
+
+// Setup mysql database
+// dbconfig.json에 설정 정보를 입력해야 함.
+var mysql = require("mysql");
+var dbconfig = require("./dbconfig.json");
+
+var connection = mysql.createConnection({
+  host: dbconfig.host,
+  user: dbconfig.user,
+  password: dbconfig.password,
+  database: dbconfig.database
+});
+
+connection.connect();
 
 // 2. Setup rules (middlewares)
 var router = express.Router();
 
+function makeRawResponse() {
+  var ret = "";
+  for (var i = 0; i < arguments.length; i++) {
+    ret += arguments[i];
+    if (i != arguments.length - 1) {
+      ret += "\n";
+    }
+  }
+  return ret;
+}
+
+function renderLink(name, link) {
+  return '<li><a href="' + link + '">' + name + "</a></li>";
+}
+
 router.get("/", function(req, res) {
-  response =
-    "Welcome to Jamquery!\n" +
-    "We have no landing page yet.\n" +
-    "/{keyword} for search\n" +
-    "/+{keyword} for add new link.";
+  response = makeRawResponse(
+    "Welcome to Jamquery!\n",
+    "We have no landing page yet.\n",
+    "/{keyword} for search\n",
+    "/+{keyword} for add new link."
+  );
 
   res.send(response);
 });
-
-function renderLink(name, link) {
-  return '<a href="' + link + '">' + name + '</a>';
-}
 
 router.get("/:keyword", function(req, res) {
   var keyword = req.params.keyword;
@@ -30,14 +56,33 @@ router.get("/:keyword", function(req, res) {
       "]"
   );
 
-  // res.send(renderLink("Google", "https://www.google.com/"))
-  res.send("You requested for keyword [" + keyword + "]");
+  connection.query(
+    "SELECT * FROM tb_jamquery WHERE name LIKE '%" + keyword + "%'",
+    function(err, rows) {
+      if (err) throw err;
+
+      links = rows
+        .map(function(row) {
+          return renderLink(row.name, row.url);
+        })
+        .join("\n");
+
+      links = "<ol>" + links + "</ol>";
+
+      var response = makeRawResponse(
+        "<p>You requested for keyword [" + keyword + "]</p>",
+        links
+      );
+
+      res.send(response);
+    }
+  );
 });
 
 // mount the router on the app
 app.use("/", router);
 
-// 3. Listening to requests
+// Listening to requests
 var args = process.argv;
 if (args.length > 2) {
   port = parseInt(args[2]);

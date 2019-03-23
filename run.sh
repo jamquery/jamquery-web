0302770c0e7f7685ec3427bc0beab379c16a04b6
tura <soldier4443@gmail.com>
2019-03-23 07:09:51 +0000

    Solve permission problems..

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # Script for turning on the server quickly.
 # Please use this script only when running a server.
 # Change the port number below to switch the port.
-npm install && npm run build && sudo npm run serve -- -p 80
+sudo npm install && sudo npm run build && sudo npm run serve -- -p 80
